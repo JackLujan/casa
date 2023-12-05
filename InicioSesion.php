4cc0e684044efc5df39afe9faf737066e91dd3d1
Angel Puentes Preciado <73569264+AngelPuentesPreciado@users.noreply.github.com>
2023-12-05 10:20:31 -0600

    Update and rename InicioSesion.html to InicioSesion.php

--- InicioSesion.php
+++ InicioSesion.php
@@ -1,9 +1,80 @@
+<?php
+
+//index.php
+
+//Include Configuration File
+include('config.php');
+
+$login_button = '';
+
+
+if(isset($_GET["code"]))
+{
+
+ $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+
+
+ if(!isset($token['error']))
+ {
+ 
+  $google_client->setAccessToken($token['access_token']);
+
+ 
+  $_SESSION['access_token'] = $token['access_token'];
+
+
+  $google_service = new Google_Service_Oauth2($google_client);
+
+ 
+  $data = $google_service->userinfo->get();
+
+ 
+  if(!empty($data['given_name']))
+  {
+   $_SESSION['user_first_name'] = $data['given_name'];
+  }
+
+  if(!empty($data['family_name']))
+  {
+   $_SESSION['user_last_name'] = $data['family_name'];
+  }
+
+  if(!empty($data['email']))
+  {
+   $_SESSION['user_email_address'] = $data['email'];
+  }
+
+  if(!empty($data['gender']))
+  {
+   $_SESSION['user_gender'] = $data['gender'];
+  }
+
+  if(!empty($data['picture']))
+  {
+   $_SESSION['user_image'] = $data['picture'];
+  }
+ }
+}
+
+
+if(!isset($_SESSION['access_token']))
+{
+
+ $login_button = '<a href="'.$google_client->createAuthUrl().'">Login With Google</a>';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacto - Casa de Cambio</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -95,6 +166,22 @@
 
             <button type="submit">Registrarse</button>
             <br>
+            <div class="container">
+                <br />
+                <br />
+                <div class="panel panel-default">
+                <?php
+                if($login_button == '')
+                {
+                    echo "<script> alert('Usuario registrado.');window.location='servicios.php' </script>";
+                }
+                else
+                {
+                    echo '<div align="center">'.$login_button . '</div>';
+                }
+                ?>
+                </div>
+                </div>
             <br>
             <a href="Login.html">Ya tienes cuenta? Inicia sesion!</a>
         </form>
