99d05515eb6d74f5830d655ca3a229f31169b7c1
Angel Puentes Preciado <73569264+AngelPuentesPreciado@users.noreply.github.com>
2023-12-05 19:53:41 -0700

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -19,9 +19,20 @@
             text-align: center;
             padding: 1rem;
         }
+        header input{
+            height: 50px;
+            width: 150px;
+            background-color: #258d19;
+            color: #fff;
+            border:none;
+            border-radius:10px;
+        }
+        header input:hover{
+            background-color: #4ea93b;
+        }
 
         nav {
-            background-color: #f8f9fa;
+            background-color: #b4ff9a;
             padding: 0.5rem;
         }
 
@@ -31,6 +42,17 @@
             margin: 0 1rem;
             font-weight: bold;
         }
+        nav button{
+            height: 50px;
+            width: 150px;
+            background-color: #258d19;
+            color: #fff;
+            border:none;
+            border-radius:10px;
+        }
+        nav button:hover{
+            background-color: #4ea93b;
+        }
 
         main {
             padding: 2rem;
@@ -39,6 +61,9 @@
         .welcome-section {
             text-align: center;
             padding: 2rem;
+            border: 2px solid #92e27a;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+            border-radius: 10px;
         }
 
         .card-container {
@@ -54,7 +79,7 @@
             border-radius: 8px;
             padding: 1.5rem;
             margin: 1rem;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
             text-align: center;
         }
 
@@ -63,7 +88,22 @@
         }
 
         .service-section {
+            text-align: center;
             margin-top: 2rem;
+            border: 2px solid #92e27a;
+            box-shadow: 0 0 10px rgba(0, 0, 0 , 0.3);
+            border-radius: 10px
+        }
+        .service-section button{
+            height: 50px;
+            width: 150px;
+            background-color: #258d19;
+            color: #fff;
+            border:none;
+            border-radius:10px;
+        }
+        .service-section button:hover{
+            background-color: #4ea93b;
         }
 
         .service {
@@ -82,6 +122,13 @@
             text-align: center;
             padding: 1rem;
         }
+        .geo{
+            border: 2px solid #92e27a;
+            margin-left: 15rem;
+            margin-right: 15rem;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        }
         #consultar{
             width: 500px;
             height: 100px;
@@ -94,16 +141,16 @@
 <body>
 
     <header>
+        <form action="logout.php"> 
+            <input type="submit" name="cerrar" id="cerrar" value="Cerrar sesion">
+        </form>
         <h1>Casa de Cambio</h1>
     </header>
 
     <nav>
-        <form action="logout.php"> 
-            <input type="submit" name="cerrar" id="cerrar" value="Cerrar sesion">
-        </from>
-        <a href="Cotizaciones.php">Cotizaciones</a>
-        <a href="contactanos.html">Contacto</a>
-        <a href="InicioSesion.php">InicioSesion/Login</a>
+        <a href="Cotizaciones.html"><button>Cotizaciones</button></a>
+        <a href="contactanos.html"><button>Contacto</button></a>
+        <a href="InicioSesion.php"><button>InicioSesion/Login</button></a>
     </nav>
 
     <main>
@@ -112,64 +159,19 @@
             <p>Ofrecemos servicios de cambio de divisas con tasas competitivas.</p>
         </section>
 
-        <section id="cotizaciones">
-            <h2>Cotizaciones</h2>
-            <p>Te cotizamos con el tipo de efectivo que desees</p>
-            <form action="Cotizaciones.php" method="post">
-                <input type="submit" name="consultar" id="consultar" value="Ver Cotizaciones">
-            </form>
-        </section>
-
         <section class="service-section" id="servicios">
-            <h2>Servicios</h2>
-
-            <div class="service">
-                <div class="service-icon">🌍</div>
-                <div class="service-info">
-                    <h3>Cambio de Divisas</h3>
-                    <p>Ofrecemos tasas competitivas para cambiar diversas monedas extranjeras.</p>
-                </div>
-            </div>
-
-            <div class="service">
-                <div class="service-icon">💵</div>
-                <div class="service-info">
-                    <h3>Compra y Venta</h3>
-                    <p>Facilitamos la compra y venta de monedas extranjeras para tus necesidades financieras.</p>
-                </div>
-            </div>
-
-            <div class="service">
-                <div class="service-icon">🔄</div>
-                <div class="service-info">
-                    <h3>Transferencias Internacionales</h3>
-                    <p>Realizamos transferencias internacionales de fondos de manera segura y eficiente.</p>
-                </div>
-            </div>
+                            <h3>Cambio de Divisas</h3>
+                            <p>Ofrecemos tasas competitivas para cambiar diversas monedas extranjeras.</p>
+                            <a href="divisas.html"><button>Compra de divisas</button></a>
+                            <p></p>
+                    </div>
 
         </section>
 
-        <section id="contacto">
-            <h2>Contacto</h2>
-            <!-- Aquí puedes agregar un formulario de contacto o información de contacto -->
-        </section>
-
-        <div class="card-container">
-            <div class="card">
-                <h3>Cambio de efectivo</h3>
-                <p>Cambio de dinero por peso, dolar, euro.</p>
-            </div>
-            <div class="card">
-                <h3>Compra</h3>
-                <p>Compra de divisas que no necesites.</p>
-            </div>
-            <div class="card">
-                <h3>Venta</h3>
-                <p>venta de divisas en buen estado.</p>
-            </div>
-        </div>
     </main>
     <center>
+        <section class="geo">
+        <h2>Encuentranos aqui!</h2>
         <iframe
         width="600"
         height="450"
@@ -180,7 +182,7 @@
     ></iframe>
 
     <p>O puedes abrir el enlace directamente en <a href="https://maps.app.goo.gl/4VKSss4T2bMHqFRw9" target="_blank">Google Maps</a>.</p>
-
+    </section>
 
     </center>
     <footer>
@@ -189,3 +191,4 @@
 
 </body>
 </html>
+
